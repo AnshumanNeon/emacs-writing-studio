3d30736bc39e5156bfcdf197b6940bbe74437a41
AnshumanNeon <anshumanneon@gmail.com>
2026-02-13 19:37:54 +0530

    add fullscreen for my system (works on my system)

--- init.el
+++ init.el
@@ -80,6 +80,10 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 
+;; full-screen
+
+(set-frame-parameter nil 'fullscreen 'fullboth)
+
 ;; Short answers only please
 
 (setq-default use-short-answers t)
